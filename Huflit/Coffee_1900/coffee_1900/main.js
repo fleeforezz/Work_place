318e3a5bc9f90a1186dc61c5dc11c25071d15060
Trương Minh Nhật <fleeforezz@gmail.com>
2023-10-26 12:05:49 +0700

    Update

--- Huflit/Coffee_1900/coffee_1900/main.js
+++ Huflit/Coffee_1900/coffee_1900/main.js
@@ -1,3 +1,4 @@
+// Exquisite
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -21,5 +22,32 @@ function showSlides(n) {
   for (i=0; i<slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slides[slideIndex -1].style.display = "block";
+  slides[slideIndex - 1].style.display = "block";
+}
+
+// Customer Says
+let clickIndex = 1;
+clickSlides(clickIndex);
+
+function nextSlides(m) {
+  clickSlides(clickIndex += m);
+}
+
+function currentClick(m) {
+  clickSlides(clickIndex = m);
+}
+
+function clickSlides(m) {
+  let j;
+  let click = document.getElementsByClassName("customer-says-message");
+  if (m > click.length) {
+    clickIndex = 1;
+  }
+  if (m < 1) {
+    clickIndex = click.length;
+  }
+  for (j=0; j<click.length; j++) {
+    click[j].style.display = "none";
+  }
+  click[clickIndex - 1].style.display = "block";
 }
