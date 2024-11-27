b94adb271fd6e44ee74944db5128434d590292c3
Trương Minh Nhật <fleeforezz@gmail.com>
2024-11-27 13:08:57 +0700

    Update hero

--- Personal_projects/Portfolio_3.0/src/app/components/layout/Hero.js
+++ Personal_projects/Portfolio_3.0/src/app/components/layout/Hero.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Hero() {
     return (
         <section className="flex flex-col gap-[5rem] px-[20px]">
@@ -9,7 +11,7 @@ export default function Hero() {
             </div>
 
             <div className="container_2">
-                <h2 className="ml-[50px] text-[3.5rem] text-[--heading] leading-[3.5rem] font-semibold">
+                <h2 className="ml-[50px] text-[3vw] text-[--heading] leading-[3.5rem] font-semibold">
                     Software <br></br>
                     Engineer
                 </h2>
@@ -20,6 +22,23 @@ export default function Hero() {
                         *Available for work
                     </span>
                 </span>
+            </div>
+
+            <div className="container_3 flex justify-between">
+                <div>
+                    <p className="text-[1.5vw] text-[--heading] font-medium">
+                        Also I'm a
+                    </p>
+                    <h2 className="text-[6vw] text-[--heading] font-semibold">
+                        Web Designer
+                    </h2>
+                </div>
+                <div>
+                    <Image src={"arrow-up-right.svg"} width={32} height={32} alt="arrow-up-right" />
+                    <p>
+                        More about me
+                    </p>
+                </div>
             </div>
         </section>
     )
